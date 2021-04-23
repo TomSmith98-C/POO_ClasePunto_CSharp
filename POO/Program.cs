f2154cfbf99a366a841acdcf56072c091720ca8c
Tom Smith <74121145+TomSmith98-C@users.noreply.github.com>
2021-04-23 13:43:30 -0500

    Add files via upload

---
+++ POO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Realizar_Tarea();
+        }
+        static void Realizar_Tarea() {
+
+            Punto origen = new Punto();
+            Console.WriteLine("El origen es de:" + origen.Mostrar());
+            origen.setX(2);
+            origen.setY(6);
+            Console.WriteLine("El nuevo punto es de:" + origen.Mostrar());
+
+            Punto PHasta = new Punto(4,5);
+            Console.WriteLine("El destino es de:" + PHasta.Mostrar());
+            Console.WriteLine("La distancia entre los puntos es de:"+ origen.DistanciaHasta(PHasta));
+
+
+
+        }
+    }
+}
---
+++ POO/Punto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace POO
+{
+    public class Punto
+    {
+        int x,y;
+
+       public Punto()
+        {
+            x = 0;
+            y = 0; 
+        }
+
+        public Punto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void Nuevo(int x, int y)
+        {
+            this.x = x;
+            this.y = y; 
+        }
+        public void setX(int x)
+        {
+            this.x = x; 
+        }
+
+        public void setY(int y)
+        {
+            this.y = y;
+        }
+
+        public int getX()
+        {
+            return x; 
+        }
+        public int getY()
+        {
+            return y;
+        }
+
+        public string Mostrar() {
+
+            return "(" + x.ToString() + "," +y.ToString()+").";
+        }
+
+        public string DistanciaHasta(Punto hasta)
+        {
+            int xdif = hasta.x - this.x ;
+            int ydif = hasta.y - this.y ;
+            xdif = Math.Abs(xdif);
+            ydif = Math.Abs(ydif);
+
+            return "(" + xdif.ToString() + "," + ydif.ToString() + ").";
+        }
+    }
+}
